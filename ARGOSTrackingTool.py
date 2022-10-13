235e59661989c5e5f52c77fef8bf9e28ef191137
AnnabelleWhite <annabelle.white@duke.edu>
2022-10-13 14:47:14 -0400

    Update ARGOSTrackingTool.py
    Task 8 errors

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -61,6 +61,10 @@ for the_key, the_value in date_dict.items():
       #Add matching keys to list
       matching_keys.append(the_key)
 
+#Report whether no keys were found
+if len(matching_keys) == 0:
+    print(f"Sara was not located on {user_date}.")
+
 #Iterate through those keys
 for matching_key in matching_keys:
     obs_lat, obs_lon = location_dict[matching_key]
